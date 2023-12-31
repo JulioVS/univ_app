5ed8cc5433bc3e2df9de228255b4cd4c556572e8
Julio Errecart <julioer@me.com>
2023-12-31 16:12:56 -0300

    Add partials to refactor main view

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
 
-  validates :short_name,  presence: true, length: { minimum: 3,  maximum: 15  } 
-  validates :name,        presence: true, length: { minimum: 5,  maximum: 50  } 
+  validates :short_name,  presence: true, length: { minimum:  3, maximum:  15 } 
+  validates :name,        presence: true, length: { minimum:  5, maximum:  50 } 
   validates :description, presence: true, length: { minimum: 10, maximum: 300 } 
-  
+
 end
